4cf642ca73884e24c5a24f030b17b3f928fb0f0c
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-10-16 10:48:18 -0700

    Import plt, np, pd, sns

--- HMXB/__main__.py
+++ HMXB/__main__.py
@@ -1,4 +1,7 @@
-import pandas
+import matplotlib.pyplot as plt
+import numpy             as np
+import pandas            as pd
+import seaborn           as sns
 
 def main() -> int:
         return 0
