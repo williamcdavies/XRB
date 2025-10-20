05e1401fe1c8e672c26f1913ed7f573c0070b69c
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-10-19 19:05:38 -0700

    Create __main__.py

---
+++ XRB/__main__.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy             as np
+import pandas            as pd
+import seaborn           as sns
+
+def main() -> int:
+        return 0
+
+if __name__ == "__main__":
+        main()
