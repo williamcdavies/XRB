9d1b326f422bb77a61e87f32a96363cd0b616512
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-11-09 15:16:53 -0800

    Remove load_dotenv
    load_dotenv did not solve database connection errror. Removing load_dotenv did not change the error state. load_dotenv was redundant

--- server/config/settings.py
+++ server/config/settings.py
@@ -10,14 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from dotenv  import load_dotenv
 from pathlib import Path
 
 import os
-
-# Reads variables from a .env file and sets them in os.environ 
-# (from https://pypi.org/project/python-dotenv/)
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
