7bace383150c007cbbab16afb633dd0b2071562e
Ethan Claire <ethanedwardclaire@gmail.com>
2025-11-11 13:47:24 -0800

    Final? Cleaned Original Data

--- data/original_data/cleanup_data_xrb.py
+++ data/original_data/cleanup_data_xrb.py
@@ -2,15 +2,15 @@ import pandas as pd
 
 #data cleanup:
 #replacing '-9000' entries with the float representation of infinity,
-#'unknown' entries with 'N/A'
+#'unknown' entries with NULL
 def cleanup(filename):
     df = pd.read_csv(filename, dtype=str)  # everything will be strings, conversion will be done during visualization
 
     df = df.replace('-9000', float('inf'))
-    df = df.replace('unknown', 'N/A')
+    df = df.replace('unknown', 'NULL')
 
     df.to_csv('clean_data/xrb_properties_CLEAN.csv', index=False)
     return df
 
-df = cleanup('xrb_properties.csv')
+df = cleanup('original_data/xrb_properties.csv')
 
