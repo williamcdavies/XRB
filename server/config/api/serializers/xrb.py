daf7f046599977bd3b65f98845a1190c7d156993
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-01 13:35:43 -0800

    Link xrb model to xrb serializer

--- server/config/api/serializers/xrb.py
+++ server/config/api/serializers/xrb.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import 
-from rest_framework             import serializers
+from server.config.api.models import xrb
+from rest_framework           import serializers
 
 
 class XRBSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = XRB
+        model = xrb
         fields = ["name", "distance", "distance_err", "rl", "incl", "incl_err", "hard_line_slope", "hard_line_slope_err", "spec_type", "p_orb", "mass"]
