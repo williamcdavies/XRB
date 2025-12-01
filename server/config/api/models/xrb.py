e53a38954050dabc0bba67faa25c88d5400256d1
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-01 13:26:37 -0800

    Rename xrb_table.py to xrb.py

---
+++ server/config/api/models/xrb.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+
+class XRB(models.Model):
+    name = models.CharField(
+        max_length=256,
+        primary_key=True
+    )
+
+    distance = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    distance_err = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    rl = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True
+    )
+
+    incl = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    incl_err = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    hard_line_slope = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    hard_line_slope_err = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    spec_type = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True
+    )
+
+    p_orb = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    mass = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = "xrb_properties"
+
+    def __str__(self):
+        return self.name
