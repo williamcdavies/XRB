a9b1c7e209016ab2272a9acbc6ebabb0abf6a028
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-01 13:28:16 -0800

    Match field names to model fields

--- server/config/api/serializers/lrlx.py
+++ server/config/api/serializers/lrlx.py
@@ -5,4 +5,4 @@ from rest_framework             import serializers
 class LRLXSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = LRLX
-        fields = ["name", "lrlx_class", "lr", "lr_ler", "lr_uer", "lx", "lx_ler", "lx_uer", "uplink", "alpha", "nu", "e1_measured", "e2_measured", "gamma", "time", "ref"]
+        fields = ["name", "class", "lr", "lr_ler", "lr_uer", "lx", "lx_ler", "lx_uer", "uplim", "alpha", "nu", "e1_measured", "e2_measured", "gamma", "time", "ref", "id"]
--- server/config/api/serializers/xrb.py
+++ server/config/api/serializers/xrb.py
@@ -5,4 +5,4 @@ from rest_framework             import serializers
 class XRBSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = XRB
-        fields = ["name", "distance", "dist_err", "rl", "incl", "incl_err", "hard_slope", "hard_slope_err", "spec_type", "porb", "mass"]
+        fields = ["name", "distance", "distance_err", "rl", "incl", "incl_err", "hard_line_slope", "hard_line_slope_err", "spec_type", "p_orb", "mass"]
