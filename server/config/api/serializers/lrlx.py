7848be97784b35635dc79f154d8c791bcaab3461
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-01 13:35:37 -0800

    Link lrlx model to lrlx serializer

--- server/config/api/serializers/lrlx.py
+++ server/config/api/serializers/lrlx.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import 
-from rest_framework             import serializers
+from server.config.api.models import lrlx
+from rest_framework           import serializers
 
 
 class LRLXSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = LRLX
+        model = lrlx
         fields = ["name", "class", "lr", "lr_ler", "lr_uer", "lx", "lx_ler", "lx_uer", "uplim", "alpha", "nu", "e1_measured", "e2_measured", "gamma", "time", "ref", "id"]
