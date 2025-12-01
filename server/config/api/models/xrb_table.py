b01d79bf9abb237929be82123e6fa75d46917701
Ethan Claire <ethanedwardclaire@gmail.com>
2025-12-01 13:09:14 -0800

    update name to be the same as postgres

--- server/config/api/models/xrb_table.py
+++ server/config/api/models/xrb_table.py
@@ -1,78 +1,78 @@
 from django.db import models
 
 
-class XRBTable(models.Model):
+class XRB(models.Model):
 
     name = models.CharField(
         max_length=255,
         null=True,
         blank=True,
-        db_column="Name",       
+        db_column="name",       
     )
 
     dist = models.FloatField(
         null=True,
         blank=True,
-        db_column="Dist",
+        db_column="distance",
     )
 
     distErr = models.FloatField(
         null=True,
         blank=True,
-        db_column="DistErr",
+        db_column="dist_err",
     )
 
     rl = models.CharField(
         max_length=50,
         null=True,
         blank=True,
-        db_column="RL",
+        db_column="rl",
     )
 
     incl = models.FloatField(
         null=True,
         blank=True,
-        db_column="Incl",
+        db_column="incl",
     )
 
     incl_err = models.FloatField(
         null=True,
         blank=True,
-        db_column="InclErr",
+        db_column="incl_err",
     )
 
     hardline_slope = models.FloatField(
         null=True,
         blank=True,
-        db_column="HardlineSlope"
+        db_column="hard_slope"
 
     hardline_slope_err = models.FloatField(
         null=True,
         blank=True,
-        db_column="HardlineSlopeErr",
+        db_column="hard_slope_err",
     )
 
     spec_type = models.CharField(
         max_length=100,
         null=True,
         blank=True,
-        db_column="SpecType",
+        db_column="spec_type",
     )
 
     porb = models.FloatField(
         null=True,
         blank=True,
-        db_column="Porb",
+        db_column="porb",
     )
 
     mass = models.FloatField(
         null=True,
         blank=True,
-        db_column="Mass",
+        db_column="mass",
     )
 
     class Meta:
-        db_table = "xrb_table"
+        db_table = "xrb"
 
     def __str__(self):
         return self.name or f"XRB #{self.pk}"
