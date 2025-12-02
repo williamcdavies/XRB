c1c0bd08af2aad75f90785ba7faf116d722d13ff
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-01 17:54:03 -0800

    Fixed import typo (capitalisation)

--- server/api/serializers/lrlx.py
+++ server/api/serializers/lrlx.py
@@ -1,8 +1,8 @@
-from api.models import LRLX
-from rest_framework           import serializers
+from api.models     import lrlx
+from rest_framework import serializers
 
 
 class LRLXSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = LRLX
+        model = lrlx
         fields = ["name", "classification", "lr", "lr_ler", "lr_uer", "lx", "lx_ler", "lx_uer", "uplink", "alpha", "nu", "e1_measured", "e2_measured", "gamma", "time", "ref", "id"]
--- server/api/serializers/xrb.py
+++ server/api/serializers/xrb.py
@@ -1,8 +1,8 @@
-from api.models import XRB
-from rest_framework           import serializers
+from api.models     import xrb
+from rest_framework import serializers
 
 
 class XRBSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = XRB
+        model = xrb
         fields = ["name", "distance", "distance_err", "rl", "incl", "incl_err", "hard_line_slope", "hard_line_slope_err", "spec_type", "p_orb", "mass"]
