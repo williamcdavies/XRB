ecf80c093b8bbe84dc2eecfe84baaf01b6c8f20f
jaketheduque <jxie7488@gmail.com>
2025-12-01 19:02:09 -0800

    added name parameter filtering in GET query

--- server/modules/api/views/lrlx.py
+++ server/modules/api/views/lrlx.py
@@ -6,3 +6,14 @@ from modules.api.serializers.lrlx import LRLXSerializer
 class LRLXViewset(viewsets.ModelViewSet):
     queryset = LRLX.objects.all()
     serializer_class = LRLXSerializer
+    
+    def get_queryset(self):
+        queryset = LRLX.objects.all()
+        
+        # if a name is provided in the query URL, filter using that
+        name = self.request.query_params.get('name', None)
+        
+        if name is not None:
+            queryset = queryset.filter(name=name)
+            
+        return queryset
--- server/modules/api/views/xrb.py
+++ server/modules/api/views/xrb.py
@@ -6,3 +6,14 @@ from modules.api.serializers.xrb import XRBSerializer
 class XRBViewset(viewsets.ModelViewSet):
     queryset = XRB.objects.all()
     serializer_class = XRBSerializer
+    
+    def get_queryset(self):
+        queryset = XRB.objects.all()
+        
+        # if a name is provided in the query URL, filter using that
+        name = self.request.query_params.get('name', None)
+        
+        if name is not None:
+            queryset = queryset.filter(name=name)
+            
+        return queryset
