aec19230bffb5ce842efc3cf679885a5d4160f5e
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-02 15:30:55 -0800

    Modify whitespace

--- server/config/urls.py
+++ server/config/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 from rest_framework.routers import DefaultRouter
 
 from modules.api.views.xrb import XRBViewset
