5f72ca92a67eb2fc4d44050ad5d390f438e24d01
jaketheduque <jxie7488@gmail.com>
2025-12-01 16:58:49 -0800

    added REST API endpoints

--- server/config/urls.py
+++ server/config/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from api.views.xrb import XRBViewset
+from api.views.lrlx import LRLXViewset
+
+# Create a router and register viewsets
+router = DefaultRouter()
+router.register(r'xrb', XRBViewset, basename='xrb')
+router.register(r'lrlx', LRLXViewset, basename='lrlx')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
