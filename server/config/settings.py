60b1cfa30a9cd567702f07d23b40490161b7bcb5
jaketheduque <jxie7488@gmail.com>
2025-12-08 18:11:32 -0800

    moved docker-compose.yaml to root level to also start vue

--- server/config/settings.py
+++ server/config/settings.py
@@ -17,7 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -61,6 +60,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,19 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-# Set options and positional parameters 
-# (from https://www.ibm.com/docs/en/zos/3.2.0?topic=descriptions-set-set-unset-command-options-positional-parameters)
-# - -o: 
-#     - Sets a shell flag.
-# - errexit:
-#     - Tells a noninteractive shell to execute the ERR trap and then exit. This flag is disabled when reading 
-#       profiles.
-# - xtrace:
-#     - Prints commands and their arguments as they run.
-set -o errexit
-set -o xtrace
+python manage.py collectstatic --noinput
 
-# (from https://docs.djangoproject.com/en/4.2/ref/django-admin/#migrate)
+python manage.py makemigrations
 python manage.py migrate
 
-# (from https://docs.djangoproject.com/en/4.2/ref/django-admin/#runserver)
-python manage.py runserver 0.0.0.0:8080
+gunicorn --bind 0.0.0.0:8000 config.wsgi:application
