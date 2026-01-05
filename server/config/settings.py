3e48ada3a6aa0df222cd193e155ff1cf5a7928c7
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-05 04:39:20 -0800

    Modify whitespace

--- server/config/settings.py
+++ server/config/settings.py
@@ -14,8 +14,10 @@ from pathlib import Path
 
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -143,6 +145,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
