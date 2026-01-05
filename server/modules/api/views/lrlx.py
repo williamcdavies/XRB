bff52a0ef9a6f1d94f9a86ac563cfc3ae12bc1d5
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-05 04:23:52 -0800

    Modify whitespace and clarify comment

--- server/modules/api/views/lrlx.py
+++ server/modules/api/views/lrlx.py
@@ -1,16 +1,18 @@
 from rest_framework import viewsets
 
-from modules.api.models.lrlx import LRLX
+
+from modules.api.models.lrlx      import LRLX
 from modules.api.serializers.lrlx import LRLXSerializer
 
+
 class LRLXViewset(viewsets.ModelViewSet):
-    queryset = LRLX.objects.all()
+    queryset         = LRLX.objects.all()
     serializer_class = LRLXSerializer
     
     def get_queryset(self):
         queryset = LRLX.objects.all()
         
-        # if a name is provided in the query URL, filter using that
+        # if a name is provided in the query URL, filter by name
         name = self.request.query_params.get('name', None)
         
         if name is not None:
--- server/modules/api/views/xrb.py
+++ server/modules/api/views/xrb.py
@@ -1,16 +1,18 @@
 from rest_framework import viewsets
 
-from modules.api.models.xrb import XRB
+
+from modules.api.models.xrb      import XRB
 from modules.api.serializers.xrb import XRBSerializer
 
+
 class XRBViewset(viewsets.ModelViewSet):
-    queryset = XRB.objects.all()
+    queryset         = XRB.objects.all()
     serializer_class = XRBSerializer
     
     def get_queryset(self):
         queryset = XRB.objects.all()
         
-        # if a name is provided in the query URL, filter using that
+        # if a name is provided in the query URL, filter by name
         name = self.request.query_params.get('name', None)
         
         if name is not None:
