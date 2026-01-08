6d4102be412a97e8f3ea0ef82168c7bbf4335573
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-07 19:23:31 -0800

    Updated paths to match new file structure
    App moves up a layer. Style moves up a layer.

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
-import App from './app/App.vue'
-import router from './router'
-import './styles/style.css'
+import App           from './App.vue'
+import router        from './router'
+
+import './style.css'
+
 
 createApp(App).use(router).mount('#app')
