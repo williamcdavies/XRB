038de000ee83b402981208c86eef013dfdf83310
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-07 19:24:55 -0800

    Updating router.ts to match the removal of old components

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,58 +1,14 @@
-// src/router.ts
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-import Home from './pages/Home.vue'
-import XRBPlot from './pages/XRBPlot.vue'
-import LRLXPlot from './pages/LRLXPlot.vue'
-import XRBTable from './pages/XRBTable.vue'
-import LRLXTable from './pages/LRLXTable.vue'
+import { createWebHistory, createRouter } from 'vue-router'
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/xrbplot',
-    name: 'XRB Plot',
-    component: XRBPlot,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/lrlxplot',
-    name: 'LRLX Plot',
-    component: LRLXPlot,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/xrbtable',
-    name: 'XRB Table',
-    component: XRBTable,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/lrlxtable',
-    name: 'LRLX Table',
-    component: LRLXTable,
-    meta: { requiresAuth: true }
-  }
+import Landing from './components/Landing.vue'
+
+const routes = [
+  { path: '/', component: Landing }
 ]
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(),
-  routes
-})
-
-// authentication guard
-router.beforeEach((to, _, next) => {
-  const token = localStorage.getItem('token')
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-
-  if (requiresAuth && !token) {
-    next('/')
-  } else {
-    next()
-  }
+  routes,
 })
 
 export default router
