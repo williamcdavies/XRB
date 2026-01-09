7e28867942be5065ad21282f415a326eba5ba3bf
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-08 22:29:36 -0800

    Renamed from Landing.vue. Implemented Navbar and BlurLayer Components
    Navbar listens for render events from the sign up and log in buttons. BlurLayer visibility is dependant upon either of the sign up or log in render booleans being true.

---
+++ client/src/components/landing/Hero.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import { computed, ref } from 'vue';
+    import BlurLayer from '../global/effects/BlurLayer.vue';
+    import Navbar    from './Navbar.vue'
+    
+    const renderRegisterOverlay = ref(false)
+    const renderLoginOverlay    = ref(false)
+    const renderBlurLayer       = computed(() => renderRegisterOverlay.value || renderLoginOverlay.value)
+</script>
+
+
+<template>
+    <div class="min-h-screen bg-[#121212]">
+        <!-- Navbar Component -->
+        <Navbar @register="renderRegisterOverlay = true" @login="renderLoginOverlay = true" />
+
+        <!-- Main -->
+        <main>
+        </main>
+    </div>
+
+    <!-- BlurLayer Component -->
+    <BlurLayer :visible="renderBlurLayer" />
+</template>
+
+
+<style scoped>
+</style>
