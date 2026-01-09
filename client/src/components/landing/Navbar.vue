fedd5285ce852c8fc5367e6ccd7bc3aefea845dc
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-08 22:30:22 -0800

    Create Navbar.vue
    Simple Navbar component. Template taken from daisyui.com. Buttons emit render tasks on @click events.

---
+++ client/src/components/landing/Navbar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  // Ref: https://vuejs.org/guide/components/events.html
+  const emit = defineEmits<{
+    (e: 'register'): void
+    (e: 'login'): void
+  }>()
+</script>
+
+
+<!-- Ref: https://daisyui.com/components/navbar/ -->
+<template>
+  <div class="navbar h-24 px-16">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">XRB</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal">
+        <li class="px-4"><button @click="emit('register')">Sign up</button></li>
+        <li class="px-4"><button @click="emit('login')">Log in</button></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
