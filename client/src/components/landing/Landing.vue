1cc638a2cdce53e02bfbf244ab2aec253298b192
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-09 09:24:33 -0800

    Create Landing.vue
    Attempting to write clear components. This is the root component for the landing "page". It should only contain other components and rendering logic. Specific UI/UX development should be contained entirely by each sub-component.

---
+++ client/src/components/landing/Landing.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import { computed, ref } from 'vue';
+    import BlurLayer from '../global/effects/BlurLayer.vue';
+    import Hero      from './Hero.vue';
+    import Navbar    from './Navbar.vue'
+    
+    const renderRegisterOverlay = ref(false)
+    const renderLoginOverlay    = ref(false)
+    const renderBlurLayer       = computed(() => renderRegisterOverlay.value || renderLoginOverlay.value)
+</script>
+
+
+<template>
+    <!-- Z0 -->
+    <Navbar @register="renderRegisterOverlay = true" @login="renderLoginOverlay = true" />
+    <Hero />
+
+    <!-- Z1 -->
+    <BlurLayer :visible="renderBlurLayer" />
+</template>
+
+
+<style scoped>
+</style>
