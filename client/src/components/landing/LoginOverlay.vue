326566889a500d6929336eb3936efbe9808610c2
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-09 19:55:43 -0800

    Create LoginOverlay.vue

---
+++ client/src/components/landing/LoginOverlay.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+    <div class="fixed inset-0 flex items-center justify-center">
+        
+    </div>
+</template>
+
+
+<style scoped>
+</style>
