34e3a0357028a2b8ba42c28533c02140e63768a1
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-10 17:39:17 -0800

    Create TintLayer.vue

---
+++ client/src/components/layers/TintLayer.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    // Ref: https://vuejs.org/guide/components/props.html
+    const props = defineProps<{
+        visible: boolean
+    }>()
+</script>
+
+
+<template>
+    <Transition name="fade">
+        <div v-if="visible" class="fixed inset-0 bg-black/50"></div>
+    </Transition>
+</template>
+
+
+<style scoped>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.33s;
+    }
+    
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
+    
+    .fade-enter-to,
+    .fade-leave-from {
+        opacity: 1;
+    }
+</style>
