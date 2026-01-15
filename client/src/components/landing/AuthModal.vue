fe2bcf018a0838f240c7a073990c20640fa952ee
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-14 17:04:05 -0800

    Add comments for UI blocking

--- client/src/components/landing/AuthModal.vue
+++ client/src/components/landing/AuthModal.vue
@@ -24,15 +24,18 @@
 
                     <!-- Form -->
                     <div class="flex flex-col gap-4">
+                        <!-- Continue w/ Google -->
                         <button class="group btn btn-outline btn-disabled justify-start bg-[#080808] border-white/25 text-[#cfcfcf] hover:bg-white hover:border-white hover:text-[#181818]">
                             <span class="flex items-center justify-center h-full w-8">
-                                <img class="h-4 w-4" src="../../assets/icons/google.svg"  />
+                                <img class="h-4 w-4" src="../../assets/icons/google.svg" />
                             </span>
                             <span class="text-xs tracking-wider">CONTINUE WITH GOOGLE</span>
                         </button>
+
+                        <!-- Continue w/ email -->
                         <button class="group btn btn-outline justify-start bg-transparent border-white/25 text-white hover:bg-white hover:border-white hover:text-[#181818]">
                             <span class="flex items-center justify-center h-full w-8">
-                                <img class="group-hover:invert h-5 w-5" src="../../assets/icons/mail.svg"  />
+                                <img class="group-hover:invert h-5 w-5" src="../../assets/icons/mail.svg" />
                             </span>
                             <span class="text-xs tracking-wider">CONTINUE WITH EMAIL</span>
                         </button>
