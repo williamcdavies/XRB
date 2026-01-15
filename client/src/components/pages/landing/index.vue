ae5bc1ba59ca286660f1ebd18074e5f6fc85ac46
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-15 15:41:35 -0800

    Rename Landing.vue to index.vue. Mark as directory component

--- client/src/components/pages/landing/index.vue
+++ client/src/components/pages/landing/index.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
     import { ref }   from 'vue';
 
-    // Local Components
-    import AuthModal from './AuthModal.vue';
+    // Layers
+    import BlurLayer from '../../layers/BlurLayer.vue';
+    import TintLayer from '../../layers/TintLayer.vue';
+
+    // Modals
+    import AuthModal from '../../modals/auth';
+
     import Hero      from './Hero.vue';
     import Navbar    from './Navbar.vue'
-
-    // Global Components
-    import BlurLayer from '../layers/BlurLayer.vue';
-    import TintLayer from '../layers/TintLayer.vue';
     
+
     const renderAuthModal = ref(false)
 </script>
 
@@ -18,10 +20,8 @@
     <!-- Z0 -->
     <Navbar @register="renderAuthModal = true" @login="renderAuthModal = true" />
     <Hero />
-    
     <!-- Z1 -->
     <TintLayer :visible="renderAuthModal" />
-
     <!-- Z2 -->
     <AuthModal :visible="renderAuthModal" @close="renderAuthModal = false" />
 </template>
