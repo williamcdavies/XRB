7d7b40aaadabe9f787b46a54bed138b4c422e935
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-17 16:42:30 -0800

    Let parent components handle transition
    This is a pattern I was unaware of. Parent components handle state and transitions, child components handle intent.

--- client/src/components/layers/BlurLayer.vue
+++ client/src/components/layers/BlurLayer.vue
@@ -1,31 +1,11 @@
 <script setup lang="ts">
-    // Ref: https://vuejs.org/guide/components/props.html
-    const props = defineProps<{
-        visible: boolean
-    }>()
 </script>
 
 
 <template>
-    <Transition name="fade">
-        <div v-if="visible" class="fixed inset-0 backdrop-blur-[8px]"></div>
-    </Transition>
+    <div class="fixed inset-0 backdrop-blur-[8px]"></div>
 </template>
 
 
 <style scoped>
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: opacity 0.33s;
-    }
-    
-    .fade-enter-from,
-    .fade-leave-to {
-        opacity: 0;
-    }
-    
-    .fade-enter-to,
-    .fade-leave-from {
-        opacity: 1;
-    }
 </style>
--- client/src/components/modals/auth/index.vue
+++ client/src/components/modals/auth/index.vue
@@ -1,50 +1,51 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
     import Panel_1 from './Panel_1.vue';
     import Panel_2 from './Panel_2.vue';
-    
-    // Ref: https://vuejs.org/guide/components/props.html
-    const props = defineProps<{
-        visible: boolean
-    }>()
 
     // Ref: https://vuejs.org/guide/components/events.html
     const emit = defineEmits<{
         (e: 'close'): void
     }>()
+
+    type Panel = 'panel_1' | 'panel_2'
+    const activePanel = ref<Panel>('panel_1')
 </script>
 
 
 <template>
-    <Transition name="pop">
-        <div v-if="visible" @click="emit('close')" class="fixed inset-0 flex items-center justify-center">
-            <div @click.stop class="flex flex-row min-h-[36rem] max-h-[36rem] min-w-[48rem] max-w-[48rem] bg-[#181818] overflow-hidden rounded-xl">
-                <!-- Left -->
-                <Panel_2 />
-                <!-- Right -->
-                <div class="w-1/2">
-                    <img src="../../../assets/images/steve-johnson-unsplash.jpg" class="h-full w-full object-cover">
-                </div>
+    <div @click="emit('close')" class="fixed inset-0 flex items-center justify-center">
+        <div @click.stop class="flex flex-row min-h-[36rem] max-h-[36rem] min-w-[48rem] max-w-[48rem] bg-[#181818] overflow-hidden rounded-xl">
+            <!-- Left Panel -->
+            <Transition name="slide" mode="out-in">
+                <Panel_1 v-if="activePanel === 'panel_1'" key="panel_1" @continue-with-email="activePanel = 'panel_2'" />
+                <Panel_2 v-else-if="activePanel === 'panel_2'" key="panel_2" @go-back="activePanel = 'panel_1'" />
+            </Transition>
+
+            <!-- Right Panel-->
+            <div class="w-1/2">
+                <img src="../../../assets/images/steve-johnson-unsplash.jpg" class="h-full w-full object-cover">
             </div>
         </div>
-    </Transition>
+    </div>
 </template>
 
 
 <style scoped>
-    .pop-enter-active, 
-    .pop-leave-active {
-        transition: opacity 0.33s ease, transform 0.33s ease;
+    .slide-enter-active,
+    .slide-leave-active {
+        transition: opacity 0.22s ease, transform 0.22s ease;
     }
 
-    .pop-enter-from, 
-    .pop-leave-to {
+    .slide-enter-from,
+    .slide-leave-to {
         opacity: 0;
-        transform: scale(0.75);
+        transform: translateX(-5%);
     }
 
-    .pop-enter-to, 
-    .pop-leave-from {
+    .slide-enter-to,
+    .slide-leave-from {
         opacity: 1;
-        transform: scale(1);
+        transform: translateX(0);
     }
 </style>
