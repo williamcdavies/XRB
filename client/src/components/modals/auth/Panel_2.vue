a992a209200d1f8145d4ae8f6e3210c5da1188a5
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-17 16:43:34 -0800

    Add go-back emit for for Panel_2
    This signals intent by the user to render Panel_1.vue

--- client/src/components/modals/auth/Panel_2.vue
+++ client/src/components/modals/auth/Panel_2.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+    // Ref: https://vuejs.org/guide/components/events.html
+    const emit = defineEmits<{
+        (e: 'go-back'): void
+    }>()
 </script>
 
 
@@ -7,20 +11,22 @@
         <!-- Header -->
         <div class="flex flex-col gap-4">
             <div class="flex flex-row items-center">
-                <button class="btn btn-ghost hover:bg-transparent hover:border-transparent hover:shadow-none">
+                <button @click="emit('go-back')" class="btn btn-ghost hover:bg-transparent hover:border-transparent hover:shadow-none">
                     <img class="h-4 w-4" src="../../../assets/icons/chevrons/chevron_left.svg"/>
                 </button>
                 <span class="text-2xl font-bold">Continue with email</span>
             </div>
             <span class="text-xs">We'll check if you have an account, and help create one if you don't.</span>
         </div>
+        
         <!-- Body -->
         <div class="flex flex-col gap-4">
-            <!-- Fieldset -->
+            <!-- Email fieldset -->
             <fieldset class="fieldset">
                 <legend class="fieldset-legend text-xs">Email</legend>
                 <input type="text" class="input" placeholder="Type here" />
             </fieldset>
+            
             <!-- Continue -->
             <button class="btn btn-outline bg-[#dc8c64] border-white/25 text-[#181818] hover:bg-white hover:border-white hover:text-[#181818]">
                 <span class="text-xs tracking-wider">CONTINUE</span>
