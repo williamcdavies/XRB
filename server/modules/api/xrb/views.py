bb38be457156d5582221acd8d03ce275d375edd9
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-21 10:48:52 -0800

    Create views.py

---
+++ server/modules/api/xrb/views.py
@@ -0,0 +1,19 @@
+from rest_framework                     import viewsets
+
+from server.modules.api.xrb.models      import XRB
+from server.modules.api.xrb.serializers import XRBSerializer
+
+class XRBViewset(viewsets.ModelViewSet):
+    queryset = XRB.objects.all()
+    serializer_class = XRBSerializer
+    
+    def get_queryset(self):
+        queryset = XRB.objects.all()
+        
+        # if a name is provided in the query URL, filter using that
+        name = self.request.query_params.get('name', None)
+        
+        if name is not None:
+            queryset = queryset.filter(name=name)
+            
+        return queryset
