ea1808d19c03ed3fd2592aa4c469a60a4b89f9f1
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-21 10:48:07 -0800

    Scraped old authentication paths. Updated existing paths

--- server/config/urls.py
+++ server/config/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.contrib                import admin
+from django.urls                   import include, path
 
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers        import DefaultRouter
 
-from modules.api.views.xrb import XRBViewset
-from modules.api.views.lrlx import LRLXViewset
-from modules.api.views.auth import register, logout
-from rest_framework.authtoken.views import obtain_auth_token
+from server.modules.api.xrb.views  import XRBViewset
+from server.modules.api.lrlx.views import LRLXViewset
 
 # Create a router and register viewsets
 router = DefaultRouter()
@@ -32,7 +30,4 @@ router.register(r'lrlx', LRLXViewset, basename='lrlx')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api/login/', obtain_auth_token, name='login'),
-    path('api/register/', register, name='register'),
-    path('api/logout/', logout, name='logout'),
 ]
