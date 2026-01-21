ba0103dc64e6befc1a05c60e569c0f550effa638
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-21 10:48:17 -0800

    Create serializers.py

---
+++ server/modules/api/lrlx/serializers.py
@@ -0,0 +1,8 @@
+from modules.api.lrlx.models import LRLX
+from rest_framework          import serializers
+
+
+class LRLXSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = LRLX
+        fields = ["name", "classification", "lr", "lr_ler", "lr_uer", "lx", "lx_ler", "lx_uer", "uplink", "alpha", "nu", "e1_measured", "e2_measured", "gamma", "time", "ref", "id"]
