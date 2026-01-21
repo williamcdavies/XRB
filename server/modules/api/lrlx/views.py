f8e7002dabe5cc5e30457e7320ffc7f192083629
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-21 10:48:22 -0800

    Create views.py

---
+++ server/modules/api/lrlx/views.py
@@ -0,0 +1,19 @@
+from rest_framework                      import viewsets
+
+from server.modules.api.lrlx.models      import LRLX
+from server.modules.api.lrlx.serializers import LRLXSerializer
+
+class LRLXViewset(viewsets.ModelViewSet):
+    queryset = LRLX.objects.all()
+    serializer_class = LRLXSerializer
+    
+    def get_queryset(self):
+        queryset = LRLX.objects.all()
+        
+        # if a name is provided in the query URL, filter using that
+        name = self.request.query_params.get('name', None)
+        
+        if name is not None:
+            queryset = queryset.filter(name=name)
+            
+        return queryset
