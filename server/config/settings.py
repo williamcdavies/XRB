2c742dc9e4c7be261dd192978da9b295bad1097d
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-21 10:57:57 -0800

    Update PERMISSION AND AUTHENTICATION classes for JWT auth

--- server/config/settings.py
+++ server/config/settings.py
@@ -47,12 +47,12 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ]
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
