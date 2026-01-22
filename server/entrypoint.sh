37488514b58873e9c8dd31f2ddf486b058b3a234
jaketheduque <jxie7488@gmail.com>
2026-01-22 00:37:58 -0800

    bug-fix: specify app name in entrypoint.sh commands

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -2,7 +2,7 @@
 
 python manage.py collectstatic --noinput
 
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations api
+python manage.py migrate api
 
 gunicorn --bind 0.0.0.0:8000 config.wsgi:application
