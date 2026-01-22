1171bb75ed8e017a31ec23e20c5fe96edb08bba1
jaketheduque <jxie7488@gmail.com>
2026-01-22 15:28:59 -0800

    document page added to router

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,9 +1,11 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import LandingPage from './components/pages/landing'
+import DocumentPage from './components/pages/document'
 
 const routes = [
-  { path: '/', component: LandingPage }
+  { path: '/', component: LandingPage },
+  { path: '/document', component: DocumentPage}
 ]
 
 export const router = createRouter({
