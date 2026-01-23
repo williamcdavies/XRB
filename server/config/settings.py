e6c2389ad76f77cfa675a603d5abe571f2020afd
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-23 09:27:08 -0800

    Update settings.py

--- server/config/settings.py
+++ server/config/settings.py
@@ -62,6 +62,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
