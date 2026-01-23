6ca30b39222e3319732f8a68d4cf36f67382e8b4
jaketheduque <jxie7488@gmail.com>
2026-01-22 16:30:26 -0800

    pulling data from django and displaying on desmos

--- client/src/components/pages/document/index.vue
+++ client/src/components/pages/document/index.vue
@@ -1,20 +1,51 @@
 <script setup lang="ts">
+    import axios from 'axios';
     import Desmos from 'desmos';
     import { useTemplateRef, onMounted } from 'vue';
 
     const calculatorRef = useTemplateRef('calculator');
-    // Replace 'dcb31709b452b1cf9dc26972add0fda6' with your own production API key.
-    const API_KEY = '6d296d9af7c9474a830bc30e6ab595a3';
+
+    const DESMOS_API_KEY = '6d296d9af7c9474a830bc30e6ab595a3';
+
+    async function fetchLRLXData() {
+        const response = await axios.get('http://localhost:8080/api/lrlx');
+        return response.data;
+    }
+
+    function formatScientificToLatex(value) {
+        // start by splitting the value at 'e'
+        const pair = value.split('e')
+        
+        // return scientific notation in latex
+        return `${pair[0]}*10^{${pair[1]}}`
+    }
 
     onMounted(() => {
         // TODO look at using vue-meta for cleaner handling of head tag
         const script = document.createElement('script');
-        script.src = `https://www.desmos.com/api/v1.11/calculator.js?apiKey=` + API_KEY;
+        script.src = `https://www.desmos.com/api/v1.11/calculator.js?apiKey=` + DESMOS_API_KEY;
 
         script.onload = () => {
+            // set options for desmos calculator
+            const options = {
+                expressions: true,
+                logScales: true
+            }
+
             // initialize the calculator once the script is loaded
-            const calculator = Desmos.GraphingCalculator(calculatorRef.value);
-            calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
+            const calculator = Desmos.GraphingCalculator(calculatorRef.value, options);
+
+            fetchLRLXData().then((lrlxArray) => {
+                for (var i = 0 ; i < lrlxArray.length ; i++) {
+                    const lrlxEntry = lrlxArray[i];
+                    calculator.setExpression({ 
+                        id: lrlxEntry.id, 
+                        latex: `(${formatScientificToLatex(lrlxEntry.lr.toString())}, ${formatScientificToLatex(lrlxEntry.lx.toString())})` ,
+                        label: lrlxEntry.name,
+                        showLabel: true
+                    })
+                }
+            });
         };
 
         // Add the Desmos CDN to the top of the page
@@ -23,7 +54,7 @@
 </script>
 
 <template>
-    <div ref="calculator" style="width: 600px; height: 600px;"></div>
+    <div ref="calculator" style="width: 1200px; height: 800px;"></div>
 </template>
 
 
