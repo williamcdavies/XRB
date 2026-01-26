cd8fa8dd1722286546696b8cdb9ba54df42bb922
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-26 11:20:43 -0800

    Delete LRLX and XRB endpoints

--- server/modules/api/lrlx/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views                 import LRLXViewset
-
-router = DefaultRouter()
-router.register(r'', LRLXViewset, basename='lrlx')
-
-urlpatterns = router.urls
