1f104adede1c0506b4f8d2652c3b6aef45d8df31
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-01-26 10:16:57 -0800

    Complete 'start' endpoint

--- server/modules/api/auth/views.py
+++ server/modules/api/auth/views.py
@@ -2,6 +2,7 @@ from django_otp.plugins.otp_email.models import EmailDevice
 from django.contrib.auth                 import get_user_model
 from rest_framework.decorators           import api_view, permission_classes
 from rest_framework.permissions          import AllowAny
+from rest_framework.response             import Response
 
 from modules.api.auth.serializers        import EmailSerializer
 
@@ -10,26 +11,35 @@ from modules.api.auth.serializers        import EmailSerializer
 @permission_classes([AllowAny])
 def start(request):
     # serialize start request:
-    #   start requests are expected to be the key-value pair {'email': 'email.value'}.
-    #   if start request cannot be appropriatley serialized a status.HTTP_400_BAD_REQUEST is 
+    #   start requests are expected to be of the key-value pair {'email': 'email.value'}.
+    #   if start requests cannot be appropriately serialized, a status.HTTP_400_BAD_REQUEST is 
     #       automatically raised.
-    #   'email' field is read into email following validation 
+    #   'email' is read into email following validation 
     serializer = EmailSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     email = serializer.validated_data['email']
-    email.strip().lower()
+    email = email.strip().lower()
 
     User = get_user_model()
     # lookup user by email:
-    #   if user.email==email, User user=returned_user; bool created=False
-    #   else,                 User user=returned_user; bool created=True
-    user, user_created = User.objects.get_or_create(
+    #   if user.email==email, User user=returned_user
+    #   else,                 User user=returned_user
+    user, _ = User.objects.get_or_create(
         email=email,
         defaults={'username': email},
     )
 
-    email_device, email_device_created = EmailDevice.objects.get_or_create(
+    # lookup email_device by user and name (name because users may have > 1 email_device):
+    #   if email_device.user==user 
+    #   && email_device.name=='default, EmailDevice email_device=returned_email_device
+    #   else,                           EmailDevice email_device=returned_email_device
+    email_device, _ = EmailDevice.objects.get_or_create(
         user=user,
         name='default',
         defaults={'email': email, 'confirmed': True},
     )
+
+    token = email_device.generate_token()
+    email_device.send_mail(token)
+
+    return Response()
