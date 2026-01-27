fcff5b4322f03772b4062a40d1e2a84fc1c6b53d
iamethanclaire <ethanedwardclaire@gmail.com>
2026-01-27 11:39:08 -0800

    Fixing Dashboard to Be DaisyUI Compliant
    There were some interesting errors on the other branch. I restarted and the errors have been resolved.

---
+++ client/src/components/pages/dashboard/Hero.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+    <div class="hero min-h-screen bg-[#080808]"></div>
+</template>
+
+
+<style scoped>
+</style>
--- client/src/components/pages/dashboard/index.vue
+++ client/src/components/pages/dashboard/index.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+    import Hero from './Hero.vue';
+    import Drawer from './Drawer.vue';
 </script>
 
 
 <template>
+    <Drawer />
+    <Hero />
 </template>
 
 
