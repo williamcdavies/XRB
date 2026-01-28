a08dccee88350dfd8254b6e4b82bf2d026a84aab
jaketheduque <jxie7488@gmail.com>
2026-01-27 16:32:17 -0800

    added ftpserver start command to django container entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,4 +5,8 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 
-gunicorn --bind 0.0.0.0:8000 config.wsgi:application
+# start Gunicorn to host django REST API and admin interface
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 &
+
+# start Django FTP server
+python manage.py ftpserver 0.0.0.0:10021
