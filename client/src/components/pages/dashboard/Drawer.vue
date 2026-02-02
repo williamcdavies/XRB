3dadf66b75d38676c2a1e661d5e225998ed77bb0
iamethanclaire <ethanedwardclaire@gmail.com>
2026-02-02 15:40:34 -0800

    Made Buttons all The Same Types

--- client/src/components/pages/dashboard/Drawer.vue
+++ client/src/components/pages/dashboard/Drawer.vue
@@ -10,12 +10,12 @@
                     <!-- Sidebar -->
                     <li>
                         <label for="dashboard-drawer" aria-label="open sidebar"
-                            class="btn btn-square btn-ghost is-drawer-close:tooltip is-drawer-close:tooltip-right is-drawer-open:ml-auto"
+                            class="is-drawer-close:tooltip is-drawer-close:tooltip-right is-drawer-open:ml-auto p-2 cursor-pointer"
                             data-tip="Open Sidebar">
                             <!-- Sidebar toggle icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round"
                                 stroke-linecap="round" stroke-width="1" stroke="currentColor"
-                                class="my-1.5 inline-block size-6">
+                                class="inline-block size-6">
                                 <path
                                     d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z">
                                 </path>
