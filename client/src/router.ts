7719066158a061b73b51ebccb9a427bfa43b2165
jaketheduque <jxie7488@gmail.com>
2026-02-02 22:19:30 -0800

    added about page to router

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,9 +1,11 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import LandingPage from './components/pages/landing'
+import AboutPage from './components/pages/about'
 
 const routes = [
-  { path: '/', component: LandingPage }
+  { path: '/', component: LandingPage },
+  { path: '/about', component: AboutPage },
 ]
 
 export const router = createRouter({
