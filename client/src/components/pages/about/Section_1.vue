a816cc921ad85ea680d70085d59aa09a7693ecfe
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-02-03 15:53:13 -0800

    Implement AboutPage

--- client/src/components/pages/about/Section_1.vue
+++ client/src/components/pages/about/Section_1.vue
@@ -3,7 +3,49 @@
 
 
 <template>
-    
+    <div class="flex flex-col items-center min-h-screen px-4 pt-24 pb-24 space-y-8 bg-[#080808]">
+        <!-- Text Box #1 -->
+        <div class="max-w-xl w-full">
+            <p class="text-justify leading-relaxed">
+                About
+            </p>
+        </div>
+        
+        <!-- Text Box #2 -->
+        <div class="max-w-xl w-full">
+            <p class="text-justify leading-relaxed">
+                XRB is is a CS426 Senior Project in Computer Science, Spring 2026, at UNR, CSE Department.
+            </p>
+        </div>
+        
+        <!-- Text Box #3 -->
+        <div class="max-w-xl w-full">
+            <p class="text-justify leading-relaxed">
+                Our development team: William Chuter-Davies, Ethan Claire, Jacob Xie
+            </p>
+        </div>
+
+        <!-- Text Box #4 -->
+        <div class="max-w-xl w-full">
+            <p class="text-justify leading-relaxed">
+               Instructors: Dr. Feil-Seifer (Professor of Computer Science and Engineering at the University of Nevada, Reno), Vinh Le (OIT Administrator/Instructor at University of Nevada, Reno)
+            </p>
+        </div>
+
+        <!-- Text Box #5 -->
+        <div class="max-w-xl w-full">
+            <p class="text-justify leading-relaxed">
+               Advisers: Dr. Sergiu Dascalu (Professor of Computer Science and Engineering at the University of Nevada, Reno), Dr. Richard Plotkin (Professor of Physics at the University of Nevada, Reno), Joshua Galindo (Graduate student at the University of Nevada, Reno, Department of Computer Science and Engineering), Audrey Lang (Graduate student with the University of Nevada, Reno, Department of Phyiscs)
+            </p>
+        </div>
+
+        <!-- Text Box #6 -->
+        <div class="max-w-xl w-full">
+            <p class="text-justify leading-relaxed">
+               XRB is a proof-of-concept web-hosting and data-visualisation tool proposed to the NSF by Dr. Richard Plotkin and Dr. Sergiu Dascalu et al. It is designed, developed, and published on behalf of the University of Nevada, Reno, Department of Physics and the University of Nevada, Reno, Department of Computer Science and Engineering and shall be made publicly available for academic research and education. Our team has designed and implemented support for two-dimensional data-visualisations, full-feature plots and displays, linear regression, data-mining processes, dataset I/O, dataset hosting, user authentication, and user-group management.
+            </p>
+        </div>
+    </div>
 </template>
 
 
--- client/src/components/pages/about/index.vue
+++ client/src/components/pages/about/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import Navbar from './Navbar.vue';
-
+    import Navbar from './Navbar.vue';
+    import Section_1 from './Section_1.vue';
 </script>
 
 
 <template>
     <Navbar />
+    <Section_1 />
 </template>
 
 
