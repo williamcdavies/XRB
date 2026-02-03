0b13ddfc57e4e369ba17db82a31a55f874c21a21
iamethanclaire <ethanedwardclaire@gmail.com>
2026-02-02 19:17:06 -0800

    Fixed Routers
    Routers doesn't like when you declare pages and don't use them. Removed DashboardPage and DocumentPage from router.ts

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,8 +1,6 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
-import DashboardPage from './components/pages/dashboard'
 import LandingPage from './components/pages/landing'
-import DocumentPage from './components/pages/document'
 
 const routes = [
   { path: '/', component: LandingPage }
