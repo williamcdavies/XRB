4b34b18dd3d296cafc3ea5184a367ec636a1f862
iamethanclaire <ethanedwardclaire@gmail.com>
2026-02-03 15:50:20 -0800

    Removed Non-Functioning tooltips
    Fixing in next commit

--- client/src/components/pages/dashboard/Drawer.vue
+++ client/src/components/pages/dashboard/Drawer.vue
@@ -11,8 +11,7 @@
                     <!-- Sidebar -->
                     <li>
                         <label for="dashboard-drawer" aria-label="open sidebar"
-                            class="is-drawer-close:tooltip is-drawer-close:tooltip-right is-drawer-open:ml-auto p-2 cursor-pointer"
-                            data-tip="Open Sidebar">
+                            class="is-drawer-open:ml-auto p-2 cursor-pointer">
                             <!-- Sidebar toggle icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round"
                                 stroke-linecap="round" stroke-width="1" stroke="currentColor" fill="none"
@@ -30,7 +29,7 @@
 
                     <!-- New item -->
                     <li>
-                        <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right p-2" data-tip="New">
+                        <button class="p-2" data-tip="New">
                             <!-- New icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                 stroke="currentColor" class="hover:xrb-accent-1 size-6">
@@ -43,7 +42,7 @@
 
                     <!-- Groups item -->
                     <li>
-                        <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right p-2" data-tip="Groups">
+                        <button class="p-2" data-tip="Groups">
                             <!-- Groups icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                 stroke="currentColor" class="size-6">
@@ -67,7 +66,7 @@
                     <div class="mt-auto is-drawer-open:border-t-1 border-xrb-border-1">
                         <!-- Account item -->
                         <li>
-                            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right p-2"
+                            <button class="p-2"
                                 data-tip="Account">
                                 <!-- Settings icon -->
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
