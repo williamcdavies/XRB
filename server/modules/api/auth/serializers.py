b65bc9d74f53ec712a19d192c0f5c27df4f244a2
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-02-04 11:09:51 -0800

    Write CredentialSerializer for verify()

--- server/modules/api/auth/serializers.py
+++ server/modules/api/auth/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 
 # Ref: https://www.django-rest-framework.org/api-guide/serializers/#declaring-serializers
+# Required by start()
 class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
+
+# Required by verify()
+class CredentialSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    token = serializers.CharField(min_length=6, max_length=6)
