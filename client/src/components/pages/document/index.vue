04b586862f854926bfe0b4329fe8033de47a625c
iamethanclaire <ethanedwardclaire@gmail.com>
2026-02-04 10:24:53 -0800

    Wipe document from main

--- client/src/components/pages/document/index.vue
+++ client/src/components/pages/document/index.vue
@@ -1,90 +1,9 @@
 <script setup lang="ts">
-    import axios from 'axios';
-    import { useTemplateRef, onMounted } from 'vue';
-
-    const calculatorRef = useTemplateRef('calculator');
-
-    const DESMOS_API_KEY = '6d296d9af7c9474a830bc30e6ab595a3';
-
-    async function fetchLRLXData() {
-        const response = await axios.get('http://localhost:8080/api/lrlx');
-        return response.data;
-    }
-
-    function formatScientificToLatex(value: string) {
-        // start by splitting the value at 'e'
-        const pair = value.split('e')
-        
-        // return scientific notation in latex
-        return `${pair[0]}*10^{${pair[1]}}`
-    }
-
-    onMounted(() => {
-        // TODO look at using vue-meta for cleaner handling of head tag
-        const script = document.createElement('script');
-        script.src = `https://www.desmos.com/api/v1.11/calculator.js?apiKey=` + DESMOS_API_KEY;
-
-        script.onload = () => {
-            // set options for desmos calculator
-            const options = {
-                expressions: true,
-                logScales: true,
-                xAxisScale: 'logarithmic',
-                yAxisScale: 'logarithmic'
-            }
-
-            // initialize the calculator once the script is loaded
-            // (window as any) to prevent compilation error since Desmos is loaded when page loads
-            const calculator = (window as any).Desmos.GraphingCalculator(calculatorRef.value, options);
-            
-            // initialize array of expressions that will be filled with points of lrlx data
-            let lrlxPoints = []
-
-            fetchLRLXData().then((lrlxArray) => {
-                // table defaults to hidden since we are already displaying points as expressions
-                var lrColumn = {
-                    latex: 'x',
-                    hidden: true,
-                    values: [] as string[]
-                }
-                var lxColumn = {
-                    latex: 'y',
-                    hidden: true,
-                    values: [] as string[]
-                }
-                for (var i = 0 ; i < lrlxArray.length ; i++) {
-                    const lrlxEntry = lrlxArray[i];
-
-                    // add point to table
-                    lrColumn.values.push(formatScientificToLatex(lrlxEntry.lr.toString()))
-                    lxColumn.values.push(formatScientificToLatex(lrlxEntry.lx.toString()))
-
-                    // add point as expression
-                    lrlxPoints.push({ 
-                        id: lrlxEntry.id, 
-                        latex: `(${formatScientificToLatex(lrlxEntry.lr.toString())}, ${formatScientificToLatex(lrlxEntry.lx.toString())})` ,
-                        label: lrlxEntry.name,
-                        showLabel: true
-                    })
-                }
-
-                const lrlxTable = {
-                    type: 'table',
-                    columns: [lrColumn, lxColumn]
-                }
-                calculator.setExpression(lrlxTable)
-                calculator.setExpressions(lrlxPoints)
-            });
-        };
-
-        // add the Desmos CDN to the top of the page
-        document.head.appendChild(script);
-    });
 </script>
 
 <template>
-    <div ref="calculator" style="width: 1200px; height: 800px;"></div>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+</style>
