babd3372f5692e660778c797f0966cb80e8d0160
iamethanclaire <ethanedwardclaire@gmail.com>
2026-02-04 18:07:06 -0800

    Added Home Icon
    Forgot I needed a home only until I already created the account page.

--- client/src/components/pages/dashboard/Drawer.vue
+++ client/src/components/pages/dashboard/Drawer.vue
@@ -26,6 +26,21 @@
                     <!-- Spacer -->
                     <div class="m-2"></div>
 
+                    <!-- Home item -->
+                    <li>
+                        <button class="p-4 rounded-none tooltip tooltip-right tooltip-neutral group" data-tip="Home">
+                            <!-- Home icon -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                            </svg>
+
+                            <span
+                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-hover:text-xrb-accent-1">Home</span>
+                        </button>
+                    </li>
+
                     <!-- New item -->
                     <li>
                         <button class="p-4 rounded-none tooltip tooltip-right tooltip-neutral group" data-tip="New">
@@ -70,7 +85,8 @@
                     <div class="mt-auto border-t-1 border-xrb-border-1">
                         <!-- Account item -->
                         <li>
-                            <button class="p-4 rounded-none tooltip tooltip-right tooltip-neutral group" data-tip="Account">
+                            <button class="p-4 rounded-none tooltip tooltip-right tooltip-neutral group"
+                                data-tip="Account">
                                 <!-- Account icon -->
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                     stroke="currentColor"
