1c438679e61ecc1bb9c0e54355ae7410c2357862
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-02-09 12:45:14 -0800

    Add auth guards

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,18 +1,38 @@
 import { createWebHistory, createRouter } from 'vue-router'
+import { useAuth }                        from './composables/auth'
 
-import AboutPage   from './components/pages/about'
-import LandingPage from './components/pages/landing'
-import DashboardPage from './components/pages/dashboard'
+import AboutPage                          from './components/pages/about'
+import LandingPage                        from './components/pages/landing'
+import DashboardPage                      from './components/pages/dashboard'
+import WhoAmIPage                         from './components/pages/whoami'
+
 
 const routes = [
-  { path: '/', component: LandingPage },
-  { path: '/about', component: AboutPage },
-  { path: '/dashboard', component: DashboardPage}
+  { path: '/',          component: LandingPage },
+  { path: '/about',     component: AboutPage },
+  { path: '/dashboard', component: DashboardPage, meta: { requiresAuth: true }},
+  { path: '/whoami',    component: WhoAmIPage,    meta: { requiresAuth: true }},
 ]
+
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+
+router.beforeEach(async (to) => {
+  const { isAuthenticated } = useAuth()
+
+  if(!to.meta.requiresAuth) {
+    return
+  }
+
+  if(await isAuthenticated()) {
+    return
+  } 
+
+  return '/'
 })
 
 export default router
