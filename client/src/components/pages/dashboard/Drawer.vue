f231a1c5e9ec41bf7409d9caa64234e5364966ed
iamethanclaire <ethanedwardclaire@gmail.com>
2026-02-11 10:45:29 -0800

    Change Colors for Drawer

--- client/src/components/pages/dashboard/Drawer.vue
+++ client/src/components/pages/dashboard/Drawer.vue
@@ -27,7 +27,7 @@
                             <!-- Sidebar toggle icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round"
                                 stroke-linecap="round" stroke-width="1" stroke="currentColor" fill="none"
-                                class="inline-block group-hover:stroke-xrb-accent-1 size-6 transition-colors duration-300">
+                                class="inline-block group-stroke-xrb-text-secondary group-hover:stroke-xrb-text-primary size-6 transition-colors duration-300">
                                 <path
                                     d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
                                 <path d="M9 4v16" />
@@ -41,12 +41,12 @@
                             <!-- New item icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                 stroke="currentColor"
-                                class="group-hover:stroke-xrb-accent-1 size-6 transition-colors duration-300">
+                                class="group-stroke-xrb-text-secondary group-hover:stroke-xrb-text-primary size-6 transition-colors duration-300">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
                             <span
-                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-hover:text-xrb-accent-1 leading-6">New</span>
+                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-text-xrb-text-secondary group-hover:text-xrb-text-primary leading-6">New</span>
                         </button>
                     </li>
 
@@ -57,13 +57,13 @@
                             <!-- Home icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor"
-                                class="group-hover:stroke-xrb-accent-1 size-6 transition-colors duration-300">
+                                class="group-stroke-xrb-text-secondary group-hover:stroke-xrb-text-primary size-6 transition-colors duration-300">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                             </svg>
 
                             <span
-                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-hover:text-xrb-accent-1 leading-6">Home</span>
+                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-text-xrb-text-secondary group-hover:text-xrb-text-primary leading-6">Home</span>
                         </button>
                     </li>
 
@@ -74,20 +74,20 @@
                             <!-- Groups icon -->
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                 stroke="currentColor"
-                                class="group-hover:stroke-xrb-accent-1 size-6 transition-colors duration-300">
+                                class="group-stroke-xrb-text-secondary group-hover:stroke-xrb-text-primary size-6 transition-colors duration-300">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5
                                     1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75
                                     18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
                             </svg>
                             <span
-                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-hover:text-xrb-accent-1 leading-6">Groups</span>
+                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-text-xrb-text-secondary group-hover:text-xrb-text-primary leading-6">Groups</span>
                         </button>
                     </li>
 
                     <!-- Recents -->
                     <div>
                         <span
-                            class="item-inline is-drawer-close:hidden p-4 text-xrb-accent-1 flex justify-start select-none">Recents</span>
+                            class="item-inline is-drawer-close:hidden p-4 text-xrb-text-secondary flex justify-start select-none">Recents</span>
                     </div>
 
                     <!-- Force to Bottom -->
@@ -99,13 +99,13 @@
                                 <!-- Account icon -->
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                     stroke="currentColor"
-                                    class="group-hover:stroke-xrb-accent-1 size-6 transition-colors duration-300">
+                                    class="group-stroke-xrb-text-secondary group-hover:stroke-xrb-text-primary size-6 transition-colors duration-300">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 
                                     0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 
                                     9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>
                                 <span
-                                    class="is-drawer-close:hidden select-none transition-colors duration-200 group-hover:text-xrb-accent-1 leading-6">Account</span>
+                                    class="is-drawer-close:hidden select-none transition-colors duration-200 group-text-xrb-text-secondary group-hover:text-xrb-text-primary leading-6">Account</span>
                             </button>
                         </li>
                     </div>
