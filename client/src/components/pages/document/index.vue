57a378af994fb7d98f2cc74e07a0062772153e81
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-02-13 14:57:59 -0800

    Implement dynamic container resizing

--- client/src/components/pages/document/index.vue
+++ client/src/components/pages/document/index.vue
@@ -1,13 +1,47 @@
 <script setup lang="ts">
-    import Editor  from './Editor.vue';
-    import Menubar from './Menubar.vue';
+    import { ref } from 'vue';
+    
+    import Topbar  from './Topbar.vue';
+    import Leftbar from './Leftbar.vue';
+    import Handle  from './Handle.vue';
+    import Graph   from './Graph.vue';
+
+
+    const MIN_LEFTBAR_WIDTH = 200
+    const MIN_GRAPH_WIDTH   = 200
+    const HANDLE_WIDTH      = 4
+    
+    const leftbarWidth      = ref(200)
+
+
+    function onMouseDown() {
+        window.addEventListener('mousemove', onMouseMove)
+        window.addEventListener('mouseup', onMouseUp)
+    }
+
+
+    function onMouseMove(e: MouseEvent) {
+        leftbarWidth.value = Math.min(
+            Math.max(MIN_LEFTBAR_WIDTH, e.clientX),
+            window.innerWidth - MIN_GRAPH_WIDTH - HANDLE_WIDTH
+        )
+    }
+
+
+    function onMouseUp() {
+        window.removeEventListener('mousemove', onMouseMove)
+        window.removeEventListener('mouseup', onMouseUp)
+    }
 </script>
 
 
 <template>
-    <div class="flex flex-col h-screen">
-        <Menubar class=" " />
-        <Editor class=" " />
+    <div class="grid grid-rows-[auto_1fr] h-screen w-screen"
+        :style="{ gridTemplateColumns: `${leftbarWidth}px ${HANDLE_WIDTH}px 1fr` }">
+        <Topbar class="col-span-3 h-10" />
+        <Leftbar class="row-start-2 h-full" />
+        <Handle @mousedown="onMouseDown" class="row-start-2 h-full" />
+        <Graph class="row-start-2 h-full w-full" />
     </div>
 </template>
 
