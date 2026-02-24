0a274e8c1c1b8db6a479b68c66598ec596742f0a
iamethanclaire <ethanedwardclaire@gmail.com>
2026-02-23 17:51:16 -0800

    Frontend Prototype added for all components

--- client/src/components/modals/dashboard/account-modals/ChangeNameModal.vue
+++ client/src/components/modals/dashboard/account-modals/ChangeNameModal.vue
@@ -5,7 +5,7 @@
 
 <template>
     <div class="fixed inset-0 flex items-center justify-center z-50" @click="$emit('close')">
-        <div class="select-none relative bg-xrb-menu-background rounded-3xl min-w-[30rem] max-w-[30rem] min-h-[36rem] max-h-[36rem] p-6 z-50"
+        <div class="select-none relative bg-xrb-menu-background rounded-3xl min-w-[30rem] max-w-[30rem] h-[36rem] p-6 z-50 flex flex-col"
             @click.stop>
             <div class="relative flex rounded-3xl">
                 <div @click="$emit('close')">
@@ -14,38 +14,38 @@
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
-
                 </div>
-                <p class="text-2xl absolute left-1/2 -translate-x-1/2">
+                <p class="text-2xl absolute left-1/2 -translate-x-1/2 whitespace-nowrap">
                     Change Name
                 </p>
             </div>
-            <div class="mt-10 rounded-3xl">
-                <div class="relative w-full">
-                    <input type="text" id="email" placeholder="First Name"
-                        class="peer min-w-full border border-gray-300 rounded-md px-3 pt-5 pb-2 focus:outline-none focus:border-xrb-text-accent" />
+            <!-- add border border-xrb-border-1 if you like how it looks -->
+            <div class="mt-6 rounded-xl flex-1 min-h-0 overflow-auto">
+                <div class="mt-2 relative w-full">
+                    <input type="text" id="first-name" placeholder="First Name"
+                        class="peer min-w-full border border-gray-300 rounded-md px-3 pt-2 pb-2 focus:outline-none focus:border-xrb-text-accent" />
                     <label for="First Name"
-                        class="peer absolute left-3 -top-2 bg-xrb-menu-background px-1 text-sm text-xrb-text-secondary peer-focus:text-xrb-text-accent">
+                        class="peer absolute left-2 -top-2 bg-xrb-menu-background px-1 text-sm text-xrb-text-secondary peer-focus:text-xrb-text-accent">
                         First Name
                     </label>
                 </div>
-            </div>
-            <div class="mt-6 rounded-3xl">
-                <div class="relative w-full">
-                    <input type="text" id="email" placeholder="Last Name"
-                        class="peer min-w-full border border-gray-300 rounded-md px-3 pt-5 pb-2 focus:outline-none focus:border-xrb-text-accent" />
-                    <label for="Last Name"
-                        class="peer absolute left-3 -top-2 bg-xrb-menu-background px-1 text-sm text-xrb-text-secondary peer-focus:text-xrb-text-accent">
+
+                <div class="mt-6 relative w-full">
+                    <input type="text" id="last-name" placeholder="Last Name"
+                        class="peer min-w-full border border-gray-300 rounded-md px-3 pt-2 pb-2 focus:outline-none focus:border-xrb-text-accent" />
+                    <label for="First Name"
+                        class="peer absolute left-2 -top-2 bg-xrb-menu-background px-1 text-sm text-xrb-text-secondary peer-focus:text-xrb-text-accent">
                         Last Name
                     </label>
                 </div>
+                
             </div>
-            <div class="absolute flex flex-row right-6 bottom-6 rounded-3x gap-2">
+            <div class="flex flex-row justify-end bottom-6 pt-6 rounded-3x gap-2 w-full">
                 <div @click="$emit('close')"
                     class="hover:bg-xrb-menu-background-accent px-4 py-2 rounded-3xl flex items-center justify-center cursor-pointer text-xrb-accent-1">
                     <p>Cancel</p>
                 </div>
-                <div @click="$emit('close')"
+                <div @click="$emit('save')"
                     class="bg-xrb-accent-1 px-4 py-2 rounded-3xl flex items-center justify-center cursor-pointer">
                     <p>Save</p>
                 </div>
@@ -53,7 +53,5 @@
         </div>
     </div>
 </template>
-
-
 
 <style scoped></style>
--- client/src/components/modals/dashboard/account-modals/ChangePictureModal.vue
+++ client/src/components/modals/dashboard/account-modals/ChangePictureModal.vue
@@ -5,13 +5,61 @@
 
 <template>
     <div class="fixed inset-0 flex items-center justify-center z-50" @click="$emit('close')">
-        <div class="bg-[#181818] rounded-xl min-w-[48rem] max-w-[48rem] min-h-[36rem] max-h-[36rem] p-6 z-50"
+        <div class="select-none relative bg-xrb-menu-background rounded-3xl min-w-[30rem] max-w-[30rem] h-[36rem] p-6 z-50 flex flex-col"
             @click.stop>
-            Edit Picture
+            <div class="relative flex rounded-3xl">
+                <div @click="$emit('close')">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-8">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </div>
+                <p class="text-2xl absolute left-1/2 -translate-x-1/2 whitespace-nowrap">
+                    Change Profile Picture
+                </p>
+            </div>
+            <!-- add border border-xrb-border-1 if you like how it looks -->
+            <div class="mt-6 rounded-xl flex-1 min-h-0 overflow-auto
+            flex flex-col items-center justify-center space-y-4">
+                <!-- Main Icon w/ placeholder-->
+                <img class="w-48 h-48 rounded-full object-cover"
+                    src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Rounded avatar">
+
+                <!-- Smaller Icons Under It, Row 1 -->
+                <div class="flex flex-row gap-4">
+                    <img class="w-18 h-18 rounded-full object-cover"
+                        src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Small icon">
+                    <img class="w-18 h-18 rounded-full object-cover"
+                        src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Small icon">
+                    <img class="w-18 h-18 rounded-full object-cover"
+                        src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Small icon">
+                    <img class="w-18 h-18 rounded-full object-cover"
+                        src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Small icon">
+                </div>
+
+                <!-- Row 2-->
+                <div class="flex flex-row gap-4">
+                    <img class="w-18 h-18 rounded-full object-cover"
+                        src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Small icon">
+                    <img class="w-18 h-18 rounded-full object-cover"
+                        src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Small icon">
+                    <img class="w-18 h-18 rounded-full object-cover"
+                        src="../../../../assets/images/profile-icons/profile-icon-1.jpg" alt="Small icon">
+                </div>
+            </div>
+            <div class="flex flex-row justify-end bottom-6 pt-6 rounded-3x gap-2 w-full">
+                <div @click="$emit('close')"
+                    class="hover:bg-xrb-menu-background-accent px-4 py-2 rounded-3xl flex items-center justify-center cursor-pointer text-xrb-accent-1">
+                    <p>Cancel</p>
+                </div>
+                <div @click="$emit('save')"
+                    class="bg-xrb-accent-1 px-4 py-2 rounded-3xl flex items-center justify-center cursor-pointer">
+                    <p>Save</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
-
-
 
 <style scoped></style>
