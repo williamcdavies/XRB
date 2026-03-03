b7d4e42f5c6b288b1ad96ff43908b320d13f0cde
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-03 15:48:49 -0800

    Add fade-in on page load

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,10 +3,25 @@
 
 
 <template>
-  <router-view />
+  <transition name="fade">
+    <router-view />
+  </transition>
 </template>
 
 
 <style>
-  
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 1s;
+    }
+    
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
+    
+    .fade-enter-to,
+    .fade-leave-from {
+        opacity: 1;
+    }
 </style>
