a698c5fb348336a5040a31c15dfe674f8d8c716f
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-03 15:49:09 -0800

    Set EmailBackend to SMTP

--- server/config/settings.py
+++ server/config/settings.py
@@ -83,7 +83,7 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
