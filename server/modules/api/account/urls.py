9b60ab8a49a7aff0bef310eb6c02f6837384298e
iamethanclaire <ethanedwardclaire@gmail.com>
2026-03-04 14:58:59 -0800

    Corrected URL

--- server/modules/api/account/urls.py
+++ server/modules/api/account/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 app_name    = 'account'
 urlpatterns = [
-    path('get_user_info', views.get_user_info, name='get_user_info')
+    path('get_user_info/', views.get_user_info, name='get_user_info')
 ]
