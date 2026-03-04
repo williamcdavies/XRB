7a57343ac2c9f3b764ef8f02d14f3c04f86cbbef
jaketheduque <jxie7488@gmail.com>
2026-03-03 17:43:19 -0800

    changed files to match navigation style

--- client/src/components/pages/dashboard/Drawer.vue
+++ client/src/components/pages/dashboard/Drawer.vue
@@ -69,16 +69,17 @@
 
                     <!-- Files item -->
                     <li>
-                        <router-link to="/files" class="p-4 rounded-none tooltip tooltip-right tooltip-neutral group" data-tip="Files">
+                        <button @click="handleNavigation('files')"
+                            class="p-4 rounded-none tooltip tooltip-right tooltip-neutral group" data-tip="Files">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
                                 stroke="currentColor"
-                                class="group-hover:stroke-xrb-accent-1 size-6 transition-colors duration-300">
+                                class="group-stroke-xrb-text-secondary group-hover:stroke-xrb-text-primary size-6 transition-colors duration-300">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                             </svg>
                             <span
-                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-hover:text-xrb-accent-1">Files</span>
-                        </router-link>
+                                class="is-drawer-close:hidden select-none transition-colors duration-200 group-text-xrb-text-secondary group-hover:text-xrb-text-primary leading-6">Files</span>
+                        </button>
                     </li>
 
                     <!-- Groups item -->
