18e4b1e21a4c8e4a7f0aaee77593bf37cb06b3dc
jaketheduque <jxie7488@gmail.com>
2026-03-04 23:07:08 -0800

    swapped SMTP to SSL port and EMAIL_BACKEND from console to email

--- server/config/settings.py
+++ server/config/settings.py
@@ -84,9 +84,9 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
