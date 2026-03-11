33296bd02c70021e882bfd50f00a612d13d05877
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-11 09:46:51 -0700

    Implement file preview

--- client/src/components/pages/document/Leftbar.vue
+++ client/src/components/pages/document/Leftbar.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+    import type { Table } from '@/types/table'
+    import FilePreview    from './FilePreview.vue';
+
+    
+    // Ref: https://vuejs.org/guide/components/events.html
+    const prop = defineProps<{
+        table: Table | null
+    }>()
 </script>
 
 
 <template>
-    <div class="flex flex-row h-full w-full">
+    <div class="h-full w-full overflow-hidden">
+        <FilePreview :table="prop.table" />
     </div>
 </template>
 
