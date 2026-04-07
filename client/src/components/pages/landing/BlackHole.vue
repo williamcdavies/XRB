07cca024e6b6f9bdc7ea89a13e5a1a2c01ef9f35
iamethanclaire <ethanedwardclaire@gmail.com>
2026-04-07 16:35:38 -0700

    Added custom spin + lots more rings

--- client/src/components/pages/landing/BlackHole.vue
+++ client/src/components/pages/landing/BlackHole.vue
@@ -2,15 +2,62 @@
 </script>
 
 <template>
-    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 rounded-full bg-bh-red-1 animate-pulse animate-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
-    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-94 h-94 rounded-full bg-bh-orange-1 animate-pulse animate-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+
+    <!-- Outermost circle: black -->
+    <!-- <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100 h-100 rounded-full bg-xrb-bg-1"></div> -->
+
+    <!-- Outer circles: blurry ring -->
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-92 h-92 rounded-full bg-bh-red-1 blur-3xl"></div>
+    
+    <!-- Center rings -->
+    <!-- <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-104 h-104 rounded-full bg-bh-red-1/95 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-103 h-103 rounded-full bg-bh-orange-1/95 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-101 h-101 rounded-full bg-bh-red-1/95 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-99 h-99 rounded-full bg-bh-orange-1/95 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-98 h-98 rounded-full bg-bh-red-1/95 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-97 h-97 rounded-full bg-bh-orange-1/95 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 rounded-full bg-bh-red-1/95 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+     -->
+
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-115 h-115 rounded-full bg-bh-orange-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-114 h-114 rounded-full bg-bh-orange-2 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-113 h-113 rounded-full bg-bh-orange-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-112 h-112 rounded-full bg-bh-orange-2 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-111 h-111 rounded-full bg-bh-orange-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-110 h-110 rounded-full bg-bh-orange-2 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-109 h-109 rounded-full bg-bh-red-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-108 h-108 rounded-full bg-bh-red-2 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-107 h-107 rounded-full bg-bh-yellow-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-106 h-106 rounded-full bg-bh-red-2 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-105 h-105 rounded-full bg-bh-red-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-104 h-104 rounded-full bg-bh-yellow-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-103 h-103 rounded-full bg-bh-orange-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-102 h-102 rounded-full bg-bh-yellow-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-101 h-101 rounded-full bg-bh-red-2 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100 h-100 rounded-full bg-bh-yellow-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-99 h-99 rounded-full bg-bh-orange-2 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-98 h-98 rounded-full bg-bh-orange-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-97 h-97 rounded-full bg-bh-red-1 slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 rounded-full bg-xrb-bg-1" ></div>
+
+    <!-- inner haze -->
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-96 h-96 rounded-full bg-bh-red-1/50 animate-pulse slow-spin blur-lg" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-90 h-90 rounded-full bg-xrb-bg-1 blur-sm" ></div>
 
     <!-- Inner circle: Black -->
-    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-92 h-92 rounded-full bg-xrb-bg-1">
-    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-71 h-71 rounded-full bg-bh-orange-1 animate-pulse animate-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-86 h-86 rounded-full bg-xrb-bg-1"></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-70 h-70 rounded-full bg-bh-orange-1 animate-pulse slow-spin" style="border-radius: 52% 51% 50% 49% / 50% 50% 50% 50%;" ></div>
     <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-70 h-70 rounded-full bg-xrb-bg-1"></div>
-    </div>
+    
 </template>
 
 <style scoped>
+.slow-spin {
+  animation: spin 3s linear infinite;
+}
+
+@keyframes spin {
+  from { transform: rotate(0deg); }
+  to   { transform: rotate(360deg); }
+}
 </style>
