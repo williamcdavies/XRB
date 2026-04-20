45866030cddf4f01cd104c2fda8ef78e555ea52a
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-04-20 10:24:50 -0700

    Adjust document home button to fit site theme

--- client/src/components/pages/document/Topbar.vue
+++ client/src/components/pages/document/Topbar.vue
@@ -91,13 +91,14 @@ function toggleSinusoidal(): void { emit('toggle-sinusoidal') }
     <div class="flex flex-row items-center h-10 w-full bg-xrb-bg-2 border-b border-xrb-border px-3 gap-3">
         <input ref="fileInput" type="file" accept=".csv" class="hidden" @change="handleFileInput" />
 
-        <button @click="router.push('/dashboard');"
-            text-xrb-text-secondary hover:text-xrb-text-primary
-            class="rounded-none tooltip tooltip-bottom tooltip-neutral group" data-tip="Home">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
-                stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        <button @click="router.push('/dashboard');" class="rounded-none tooltip tooltip-bottom tooltip-neutral group"
+            data-tip="Home">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                class="size-4 text-xrb-text-secondary hover:text-white transition-colors duration-150">
+                <path
+                    d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                <path
+                    d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
             </svg>
         </button>
 
