115e7a9a9bac82f6b007bc890394f02801775df8
iamethanclaire <ethanedwardclaire@gmail.com>
2026-04-20 09:37:42 -0700

    Clean up Home Button

--- client/src/components/pages/document/Topbar.vue
+++ client/src/components/pages/document/Topbar.vue
@@ -85,10 +85,10 @@ function toggleSinusoidal(): void { emit('toggle-sinusoidal') }
         <input ref="fileInput" type="file" accept=".csv" class="hidden" @change="handleFileInput" />
 
         <button @click="router.push('/dashboard');"
+            text-xrb-text-secondary hover:text-xrb-text-primary
             class="rounded-none tooltip tooltip-bottom tooltip-neutral group" data-tip="Home">
-            <!-- Home icon -->
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
-                stroke="currentColor" class="size-6 transition-colors duration-50">
+                stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
             </svg>
