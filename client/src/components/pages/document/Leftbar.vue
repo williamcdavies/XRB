b10f0f5eff65f02b4fe3f68e34bf328ae0140f1c
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-04-29 10:59:42 -0700

    Rename roggle-all-hidden to toggle-all-rows-hidden

--- client/src/components/pages/document/Leftbar.vue
+++ client/src/components/pages/document/Leftbar.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-    import type { Table }   from '@/types/table'
-    import      FilePreview from './FilePreview.vue';
+    import type { Table }    from '@/types/table'
+    import type { Group }    from '@/types/group'
+    import      FilePreview  from './FilePreview.vue'
 
 
     // Ref: https://vuejs.org/guide/components/events.html
     const prop = defineProps<{
         table:      Table | null
         hiddenRows: Set<number>
+        aColumn:    string | null
+        groups:     Map<string, Group>
     }>()
     const emit = defineEmits<{
-        (e: 'toggle-row-hidden', index: number):  void
-        (e: 'toggle-all-hidden'):                 void
-        (e: 'header', idx: number, name: string): void
+        (e: 'toggle-row-hidden', index: number):                void
+        (e: 'toggle-all-rows-hidden'):                          void
+        (e: 'header', idx: number, name: string):               void
     }>()
 </script>
 
@@ -20,7 +23,7 @@
     <div class="h-full w-full overflow-hidden">
         <FilePreview :table="prop.table" :hidden-rows="prop.hiddenRows"
             @toggle-row-hidden="(i: number) => emit('toggle-row-hidden', i)"
-            @toggle-all-hidden="emit('toggle-all-hidden')" @header="(idx, name) => emit('header', idx, name)" />
+            @toggle-all-rows-hidden="emit('toggle-all-rows-hidden')" @header="(idx, name) => emit('header', idx, name)" />
     </div>
 </template>
 
